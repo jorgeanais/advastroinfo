89c43b6f3c45176847603680e66bb08dd4cdf775
Jorge Anais <jrganais@gmail.com>
2022-07-11 16:07:18 -0400

    Just afeter class 7

--- script_pipeline.py
+++ script_pipeline.py
@@ -12,32 +12,30 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer
 
 
 FEATURES = [
-        "Amplitude",
-        "Mean",
-        "Beyond1Std",
-        "FluxPercentileRatioMid35",
-        "Freq1_harmonics_amplitude_0",
-        "Freq2_harmonics_amplitude_0",
-        "Skew",
-        "PeriodLS",
-        "SmallKurtosis",
-        "Anderson_Darling_",
-        "Stetson_K_",
-        "MaxSlope",
-    ]
+    "Amplitude",
+    "Mean",
+    "Beyond1Std",
+    "FluxPercentileRatioMid35",
+    "Freq1_harmonics_amplitude_0",
+    "Freq2_harmonics_amplitude_0",
+    "Skew",
+    "PeriodLS",
+    "SmallKurtosis",
+    "Anderson_Darling_",
+    "Stetson_K_",
+    "MaxSlope",
+]
 
 
 data = {
-        "raw": pd.read_csv(
-            "outputs/nb3/features__TESS_lightcurves_raw.csv"
-        ),
-        "median_detrended": pd.read_csv(
-            "outputs/nb3/features__TESS_lightcurves_median_after_detrended.csv"
-        ),
-        "outlier_cleaned": pd.read_csv(
-            "outputs/nb3/features__TESS_lightcurves_outliercleaned.csv"
-        ),
-    }
+    "raw": pd.read_csv("outputs/nb3/features__TESS_lightcurves_raw.csv"),
+    "median_detrended": pd.read_csv(
+        "outputs/nb3/features__TESS_lightcurves_median_after_detrended.csv"
+    ),
+    "outlier_cleaned": pd.read_csv(
+        "outputs/nb3/features__TESS_lightcurves_outliercleaned.csv"
+    ),
+}
 
 
 def replace_nan_values(X: np.ndarray) -> np.ndarray:
@@ -45,6 +43,7 @@ def replace_nan_values(X: np.ndarray) -> np.ndarray:
     Replace nan values with 0.
     """
     return np.nan_to_num(X)
+
 
 transformer_nan = FunctionTransformer(replace_nan_values)
 
@@ -57,18 +56,34 @@ df.dropna(inplace=True, how="all")
 all_features = list(df.columns.values)
 all_features.remove("type")
 all_features.remove("object_id")
+all_features.remove("AndersonDarling")
+all_features.remove("StetsonK")
 
 X = df[all_features].values
 y = df["type"].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)  #9:1
 
 # Define the procesing pipeline
-pipe = make_pipeline(StandardScaler(), transformer_nan, PCA(n_components=5), KMeans(n_clusters=7))
+pipe = make_pipeline(
+    StandardScaler(), transformer_nan, PCA(n_components=10), KMeans(n_clusters=10)
+)
 pipe.fit(X_train, y_train)
 y_pred = pipe.predict(X_test)
 
 # Print confusion matrix for test
 df = pd.DataFrame({"type": y_test, "pred": y_pred, "ones": np.ones(y_test.shape)})
-print(df.pivot_table(values='ones', index='type', columns='pred', aggfunc='sum').fillna(0))
+print(
+    df.pivot_table(values="ones", index="type", columns="pred", aggfunc="sum").fillna(0)
+)
 
+# print()
+# print()
+# print()
+
+# pipe.fit(X, y)
+# y_pred = pipe.predict(X)
+# df = pd.DataFrame({"type": y, "pred": y_pred, "ones": np.ones(y_pred.shape)})
+# print(
+#     df.pivot_table(values="ones", index="type", columns="pred", aggfunc="sum").fillna(0)
+# )
